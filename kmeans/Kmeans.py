Rewrite kmeans using numpy, precision bug fixies

kmeans/Kmeans.py
@@ -2,48 +2,41 @@ from PIL import Image
 import numpy as np
 
 
-def kmeans(data, clusters, iters):
+def kmeans(image_data, num_of_clusters, max_iter, attempts=3):
+    data_size = image_data.shape[0]
+    attempt_results = []
 
-    def get_closest_cluster(pixel_data):
-        nonlocal clusters
-        nonlocal centers
-        best_distance = 450  # max norm can not be more than this value
-        best_center = 0
-        for i in range(clusters):
-            cur_distance = np.linalg.norm(centers[i] - pixel_data)
-            if cur_distance < best_distance:
-                best_distance = cur_distance
-                best_center = i
-        return best_center
+    for attempt in range(attempts):
+        random_pixel_indices = np.random.randint(data_size, size=num_of_clusters)
+        centers = np.asarray(image_data[random_pixel_indices, :], dtype=np.float32)
+        prev_centers = np.zeros((num_of_clusters, 3))
 
-    centers = np.random.randint(0, 255, size=(clusters, 3), dtype=np.uint8)
-    prev_centers = np.ones((clusters, 3)) * -1
-    iter_count = 0
+        clusters = np.zeros(data_size)
+        distances = np.zeros((data_size, num_of_clusters))
 
-    while iter_count != iters and not np.array_equal(centers, prev_centers):
-        prev_centers = np.copy(centers)
-        pixels_in_cluster = [list() for _ in range(clusters)]
+        cur_iter = 0
+        cur_functional = 10_000
 
-        # evaluate nearest center for each pixel
-        for pixel in data:
-            pixels_in_cluster[get_closest_cluster(pixel)].append(pixel)
+        while cur_functional and cur_iter < max_iter:
+            prev_centers = np.copy(centers)
 
-        # recount centers of clusters
-        # indices and dimensions of pixels_in_cluster and centers are same
-        for i, cur_pixels_in_cluster in enumerate(pixels_in_cluster):
-            sum_of_pixels = np.zeros(3)
-            for pixel in cur_pixels_in_cluster:
-                sum_of_pixels += pixel
-            if cur_pixels_in_cluster:
-                centers[i] = sum_of_pixels / len(cur_pixels_in_cluster)
+            for i in range(num_of_clusters):
+                distances[:, i] = np.linalg.norm(image_data - centers[i], axis=1)
+            clusters = np.argmin(distances, axis=1)
 
-        iter_count += 1
+            for i in range(num_of_clusters):
+                centers[i] = np.mean(image_data[clusters == i], axis=0)
 
-    clustered_data = np.empty(data.shape, dtype=np.uint8)
-    for position, pixel in enumerate(data):
-        clustered_data[position] = centers[get_closest_cluster(pixel)]
+            cur_iter += 1
+            cur_functional = np.linalg.norm(centers - prev_centers)
 
-    return centers, clustered_data
+        attempt_results.append((cur_functional, np.asarray(centers, dtype=np.uint8), clusters))
+
+    best_answer = (10_000, [], [])
+    for attempt in attempt_results:
+        if attempt[0] < best_answer[0]:
+            best_answer = attempt
+    return best_answer[1], best_answer[2]
 
 
 """ take those images """
@@ -52,18 +45,16 @@ images = ['lena.jpg', 'grain.jpg', 'peppers.jpg']
 """ try to divide by those number of clusters """
 list_of_clusters = [2, 4, 8]
 
-""" for each (image, number of clusters) 3 attempts """
-number_of_attempts = 3
-
 for img in images:
-    image = np.array(Image.open(img), dtype=np.uint8)
+    image = np.array(Image.open(img))
     X = image.reshape((image.shape[0] * image.shape[1], image.shape[2]))
+    iteration_number = 200
 
     for cluster_number in list_of_clusters:
-        for try_number in range(number_of_attempts):
-            res_centers, new_X = kmeans(X, cluster_number, 200)
-            new_image = new_X.reshape(image.shape)
-            output_image_file = 'output_' + img + '_' \
-                                + str(cluster_number) + '_clusters_' \
-                                + str(try_number + 1) + '_attempt_' + '.jpg'
-            Image.fromarray(new_image, mode="RGB").save(output_image_file)
+        final_centers, final_clusters = kmeans(X, cluster_number, iteration_number)
+        new_X = np.vstack([final_centers[i] for i in final_clusters])
+        new_image = new_X.reshape(image.shape)
+        output_image_file = str.replace(img, '.jpg', '_') \
+            + str(cluster_number) + '_clusters.jpg'
+        Image.fromarray(new_image).save(output_image_file)
+        Image.fromarray(new_image, mode="RGB").save(output_image_file)