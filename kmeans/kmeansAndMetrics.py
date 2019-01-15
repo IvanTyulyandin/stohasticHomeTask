Fix precision error for Calinski-Harabaz index

kmeans/kmeansAndMetrics.py
@@ -30,7 +30,7 @@ def kmeans(data, num_of_clusters, max_iter, attempts=3):
             cur_iter += 1
             cur_functional = np.linalg.norm(centers - prev_centers)
 
-        attempt_results.append((cur_functional, np.asarray(centers, dtype=np.uint8), closest_clusters))
+        attempt_results.append((cur_functional, np.asarray(centers, dtype=np.uint16), closest_clusters))
 
     best_answer = (10_000, [], [])
     for attempt in attempt_results:
@@ -71,6 +71,7 @@ def calinski_harabaz_index(data, centers, closest_clusters, num_of_clusters):
     mean = np.mean(data, axis=0)
     between_disp = np.sum([len(data[closest_clusters == i]) * ((centers[i] - mean) ** 2)
                            for i in range(num_of_clusters)])
+
     return between_disp / within_disp * (data_size - num_of_clusters) / (num_of_clusters - 1)
 
 

kmeans/kmeansMetricsRunner.py
@@ -50,7 +50,7 @@ iteration_number = 200
 best_cluster_number = (best_calinski_harabaz_cluster_number + best_davies_bouldin_cluster_number) // 2
 centers, closest_clusters = kmeansAndMetrics.kmeans(X, best_cluster_number, iteration_number)
 
-new_X = np.vstack([centers[i] for i in closest_clusters])
+new_X = np.vstack([centers[i] for i in closest_clusters]).astype(np.uint8)
 new_image = new_X.reshape(image.shape)
 output_image_file = str(best_cluster_number) + '_cluster_policemen_out.jpg'
 Image.fromarray(new_image).save(output_image_file)